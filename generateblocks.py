39f83d1c4961408ba160a261cd5e72fe79b4b3f5
nathanww <nathanww@gmail.com>
2024-11-15 14:54:23 -0500

    generate valid paths from consistentStages.txt

--- generateblocks.py
+++ generateblocks.py
@@ -1,11 +1,15 @@
-infile=open("random_sample_ordered.csv")
+import os
+infile=open("consistentStages.txt") 
 inlines=infile.readlines()
 buffer=[]
 
 for line in inlines[1:]:
     splitup=line.split(",")
-    outline="./data/outputs_part4/P"+splitup[0]+"/"+splitup[1]+","+"./data/outputs_part4/P"+splitup[0]+"/"+str(int(splitup[1])-30)+","+"./data/outputs_part4/P"+splitup[0]+"/"+str(int(splitup[1])+30)+","+splitup[2]
-    buffer.append(outline)
+    outline="./data/outputs_part4/"+splitup[0]+"/"+splitup[1]+","+"./data/outputs_part4/"+splitup[0]+"/"+str(int(splitup[1])-30)+","+"./data/outputs_part4/"+splitup[0]+"/"+str(int(splitup[1])+30)+","+splitup[2]
+
+    testpath="data\\outputs_part4\\"+splitup[0]+"\\"+splitup[1]+".wav"
+    if os.path.isfile(testpath):
+        buffer.append(outline)
     if len(buffer) == 30:
         formatted=""
         for item in buffer:
